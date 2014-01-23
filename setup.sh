c1eaecc8ea41009a8269e928801037f4e59084ac
Ray Patterson <ray.patterson@akqa.com>
2014-01-22 22:00:50 -0800

    Init.

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo '.'
+echo '.'
+echo '.'
+echo '. . . Welcome'
+echo '.'
+echo '.'
+echo '.'
+echo '. . . Installing'
+echo '.'
+echo '.'
+echo '.'
+npm install -g --loglevel silent gulp bower
+npm install --save-dev --loglevel silent gulp gulp-util
+npm install --loglevel silent
+bower install --silent
+echo '.'
+echo '.'
+echo '.'
+echo '. . . Complete'
+echo '.'
+echo '.'
+echo '.'
+echo '. . . Starting'
+echo '.'
+echo '.'
+echo '.'
+gulp
